e5fb463dd005ca13a690357e475906b39f5adbfd
André Hochstein <andr.hochstein@gmail.com>
2023-04-06 16:09:43 +0200

    Add login endpoint for migration to api

--- api/handler.js
+++ api/handler.js
@@ -26,3 +26,35 @@ module.exports.crm = async (event) => {
     ),
   };
 };
+
+
+
+module.exports.login = async (event) => {
+  let body = JSON.parse(event.body);
+  let username = body?.username;
+  let password = body?.password;
+
+  if(username === "migrate-me@authfest.com" && password === "MigrateMe!") {
+    return {
+      statusCode: 200,
+      body: JSON.stringify(
+        {
+          "sub" : 123,
+          "firstname" : "Migrate",
+          "lastname" : "Me",
+          "email" : "migrate-me@authfest.com"  
+        }
+      ),
+    };
+  }
+  else {
+    return {
+      statusCode: 400,
+      body: JSON.stringify(
+        {
+          "message" : "Wrong credentials or username"
+        }
+      ),
+    } 
+  }
+};
